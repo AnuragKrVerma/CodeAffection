2f834d691db7c5f60ec6c00c3b07a1dab3ec5a18
Anurag Verma <211006@kit.ac.in>
2025-03-16 17:57:48 +0530

    First commit

---
+++ Expense Tracker/Expense Tracker/Repository/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Repository
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
